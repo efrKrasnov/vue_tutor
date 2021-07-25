db20620b2903b47732365d6515d2110fae7df969
efr_kras <efrKras@gmail.com>
2021-07-25 13:00:22 +0300

    Added gitignore to the project Created project: TaskNotes Now we can add and remove Notes Also added some stuff like dynamic style change

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const App = {
     data()  {
         return {
-            counter: 0,
-            title: 'Counter'
+            placeholderString: 'Input note name',
+            title: 'Notes List',
+            inputValue: '',
+            notes: ['Note 1', 'Note 2']
+        }
+    },
+    methods:    {
+        addNewNote()    {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        removeNote(idx)    {
+            this.notes.splice(idx, 1)
+        },
+        toUpperCase(item)   {
+            return item.toUpperCase()
+        }
+    },
+    computed: {
+        doubleCountComputed()   {
+            return this.notes.length * 2
         }
     }
 }
